8182a534b34c3960978215b0042be6b855aed91f
Renato Alves <alves.rjc+taskw@gmail.com>
2015-05-21 02:43:10 +0100

    Add a lightweight http server for local development

---
+++ serve_local.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# serve_local.sh -- created 2015-05-21, Renato Alves
+# @Last Change: 2015-05-21.
+# @Revision:    0.1
+
+# Location of the current script. Used to allow launching this from anywhere
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# The root of the HTTP Server
+cd $DIR/html 
+
+if [ $# -eq 1 ]; then
+    PORT=$1
+else
+    PORT=8000
+    echo "If you wish to run the HTTP server on a different port, pass the port as an argument to this script."
+fi
+echo "Serving on 127.0.0.1:$PORT"
+
+python -c "import BaseHTTPServer as bhs, SimpleHTTPServer as shs; bhs.HTTPServer(('127.0.0.1', $PORT), shs.SimpleHTTPRequestHandler).serve_forever()"
